70ee639b1fc44678e4480b7c5774857e3350ef5c
friendly-format-0074 <samuelagasi@gmail.com>
2021-01-22 19:29:37 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def join_nested_strings(src)
   row= 0
   while row.length< src do
     element_index= 0
-    while element< src[row].length do
+    while element.length< src[row].length do
       phrase+= src[row][element_index]
       element_index+= 1
     end
